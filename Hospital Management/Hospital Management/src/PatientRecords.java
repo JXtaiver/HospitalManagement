1e9a9df4beb0a988dd1c99e9b0a21b33497bb2a1
JXtaiver <coolhawk.joeyxt.com@gmail.com>
2025-04-28 21:03:32 -0400

    Update PatientRecords.java

--- Hospital Management/Hospital Management/src/PatientRecords.java
+++ Hospital Management/Hospital Management/src/PatientRecords.java
@@ -1,4 +1,7 @@
+import java.util.Scanner;
+
 public class PatientRecords {
+    static Scanner scan = new Scanner(System.in);
     int Patient_ID;
     int Age;
     String Patient_Name;
@@ -31,7 +34,33 @@ public class PatientRecords {
         return Age;
     }
     public void Update_Info(PatientRecords p){
-
+        System.out.println("Select an Option");
+        System.out.println("Set Name-1");
+        System.out.println("Set Age-2");
+        System.out.println("Update History-3");
+        String New;
+        int Neww;
+        int type = scan.nextInt();
+        scan.nextLine();
+        if (type>3||type<1){
+            System.out.println("Invalid number, try again.");
+            Update_Info(p);
+        }
+        switch(type){
+            case 1:
+            System.out.println("Enter updated name: ");
+            New= scan.nextLine();
+            p.setN(New);
+            break;
+            case 2:
+            System.out.println("Enter updated age");
+            Neww=scan.nextInt();
+            p.setA(Neww);
+            break;
+            case 3:
+            Update_History(p);
+            break;
+        }
     }
     public void Display_Record(PatientRecords p){
         String[] arr = p.Patient_History;
@@ -43,5 +72,15 @@ public class PatientRecords {
         System.out.println("Patient Name: "+p.getN());
         System.out.println("Patient Age: "+p.getA());
         Display_Record(p);
+    }
+    public void Update_History(PatientRecords p){
+        String[]hist= new String[p.Patient_History.length+1];
+        for(int i = 0; i<p.Patient_History.length;i++ ){
+            hist[i]=p.Patient_History[i];
+        }
+        System.out.println("Enter new History");
+        String nh = scan.nextLine();
+        hist[hist.length-1]=nh;
+        System.out.println("History Updated to: "+String.join(", ",hist));
     }
 }
