e9f1e549b716b3eddad623cb0533f4772122872c
JXtaiver <coolhawk.joeyxt.com@gmail.com>
2025-04-28 21:03:14 -0400

    Update App.java

--- Hospital Management/Hospital Management/src/App.java
+++ Hospital Management/Hospital Management/src/App.java
@@ -4,12 +4,13 @@ import java.util.Random;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
         //Create patient Records Dynamic Array
         ArrayList<PatientRecords> patients = new ArrayList<>();
         Random rand = new Random();
         //Medical history prompts
         String[] H1={"Flu", "Asthma","Diabetes", "Tuberculosis","Headache", "Allergies"};
- 
+        
         
 
     for(int i=0; i<50; i++){
@@ -25,9 +26,13 @@ public class App {
         patients.add(new PatientRecords(i, rand.nextInt(70)+1, "Patient"+i, history));
         
     }
+    PatientRecords Jake = new PatientRecords(51, 45, "Jake", H1);
+   Jake.Update_Info(Jake);
     for(PatientRecords p : patients){
         //Just Testing to see that everything works
         p.Display_Info(p);
+        
     }
+       
     }
 }
