321e729fa6d9c51fe9efb0cbbbeb814bd3af18e6
JXtaiver <coolhawk.joeyxt.com@gmail.com>
2025-04-29 00:51:15 -0400

    Update PatientRecords.java

--- Hospital Management/Hospital Management/src/PatientRecords.java
+++ Hospital Management/Hospital Management/src/PatientRecords.java
@@ -1,17 +1,53 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Random;
 
 public class PatientRecords {
+    App app = new App();
     static Scanner scan = new Scanner(System.in);
     int Patient_ID;
     int Age;
     String Patient_Name;
     String[] Patient_History;
-
+    Random rand = new Random();
+    
     public PatientRecords(int ID,int Age,String Name,String[]History){
         this.Patient_History=History;
         this.Age=Age;
         this.Patient_Name=Name;
         this.Patient_ID=ID;
+    }
+    public void addp(ArrayList<PatientRecords> patients){
+        System.out.println("Enter Name of patient");
+        scan.nextLine();
+        String n=scan.nextLine();
+        
+
+        System.out.println("Enter Patient History(enter 'done' to stop): )");
+        ArrayList<String> histo = new ArrayList<>();
+        while(true) {
+            String Entry = scan.nextLine();
+            if(Entry.equalsIgnoreCase("done")){
+                break;
+            }
+            histo.add(Entry);
+        }
+        
+        
+        String[]sl = histo.toArray(new String[0]);
+        int age = 0;
+        System.out.println("Enter Patient Age: ");
+        if(scan.hasNextInt()){
+            age = scan.nextInt();
+            scan.nextLine();
+        }else{
+            System.out.println("Invalid input, setting age to 0");
+            scan.nextLine();
+        }
+
+        
+       
+        patients.add(new PatientRecords(patients.size(), age, n, sl));
     }
     public void setN(String Name){
         this.Patient_Name=Name;
@@ -82,5 +118,56 @@ public class PatientRecords {
         String nh = scan.nextLine();
         hist[hist.length-1]=nh;
         System.out.println("History Updated to: "+String.join(", ",hist));
+        p.Patient_History=hist;
+    }
+    public void Patient_Menu(ArrayList<PatientRecords> patients){
+       System.out.println("\n Select an Option: \n 1: Display Patient List \n 2: Update Patient Info \n 3: Display Patient Info \n 4: Add Patient\n 5: Exit to main menu");
+       int type=scan.nextInt();
+       switch(type){
+        case 1:
+       
+        for(PatientRecords p : patients){
+            System.out.println("Patient ID: "+ p.getID()+ ", Name: "+p.getN());
+        }
+        Patient_Menu(patients);
+        break;
+        case 2:
+        System.out.println("Enter Patient ID: ");
+        int id = scan.nextInt(); 
+        
+ 
+        if (id >= 0 && id < patients.size()) {
+        
+            patients.get(id).Update_Info(patients.get(id)); 
+        } else {
+            System.out.println("Patient not found");
+        }
+        Patient_Menu(patients);
+        break;
+        case 3:
+        System.out.println("Enter Patient ID: ");
+        int io = scan.nextInt(); 
+        
+ 
+        if (io >= 0 && io < patients.size()) {
+        
+            patients.get(io).Display_Info(patients.get(io)); 
+        } else {
+            System.out.println("Patient not found");
+        }
+        Patient_Menu(patients);
+        break;
+        case 4:
+        addp(patients);
+        Patient_Menu(patients);
+        break;
+        case 5:
+        System.out.println("Returning to Main Menu....");
+        app.Menu(patients);
+        break;
+        default:
+        System.out.println("Invalid Option, Try again.");
+        Patient_Menu(patients);
+       }
     }
 }
