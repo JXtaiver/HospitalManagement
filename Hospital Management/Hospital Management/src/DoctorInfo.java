58b9296d53e6cb8878fd596027e063cd8bc16f6f
ultradan-beep <55604224+ultradan-beep@users.noreply.github.com>
2025-04-28 22:23:36 -0400

    Update DoctorInfo.java

--- Hospital Management/Hospital Management/src/DoctorInfo.java
+++ Hospital Management/Hospital Management/src/DoctorInfo.java
@@ -1,3 +1,36 @@
+import java.util.Random;
+
 public class DoctorInfo {
-    
+    private int doctorID;
+    private String doctorName;
+    private String[] availability;
+    private String specialization;
+
+    public DoctorInfo(int doctorID, String doctorName, String[] availability) {
+        this.doctorID = doctorID;
+        this.doctorName = doctorName;
+        this.availability = availability;
+        this.specialization = generateRandomSpecialization();
+    }
+
+    private String generateRandomSpecialization() {
+        String[] specialties = {"Cardiology", "Neurology", "Pediatrics", "Orthopedics", "Dermatology", "Oncology", "Psychiatry"};
+        Random rand = new Random();
+        return specialties[rand.nextInt(specialties.length)];
+    }
+
+    public void updateAvailability(String[] newAvailability) {
+        this.availability = newAvailability;
+    }
+
+    public void displayInfo() {
+        System.out.println("Doctor ID: " + doctorID);
+        System.out.println("Name: " + doctorName);
+        System.out.println("Specialization: " + specialization);
+        System.out.print("Availability: ");
+        for (String time : availability) {
+            System.out.print(time + " ");
+        }
+        System.out.println();
+    }
 }
