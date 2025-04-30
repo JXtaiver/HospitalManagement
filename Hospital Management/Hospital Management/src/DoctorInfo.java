e821e038b6f3edcad1ee244a32dc1158ffd16c2b
ultradan-beep <55604224+ultradan-beep@users.noreply.github.com>
2025-04-29 21:44:23 -0400

    Update DoctorInfo.java

--- Hospital Management/Hospital Management/src/DoctorInfo.java
+++ Hospital Management/Hospital Management/src/DoctorInfo.java
@@ -10,11 +10,11 @@ public class DoctorInfo {
         this.doctorID = doctorID;
         this.doctorName = doctorName;
         this.availability = availability;
-        this.specialization = generateRandomSpecialization();
+        this.specialization = this.generateRandomSpecialization();
     }
 
     private String generateRandomSpecialization() {
-        String[] specialties = {"Cardiology", "Neurology", "Pediatrics", "Orthopedics", "Dermatology", "Oncology", "Psychiatry"};
+        String[] specialties = new String[]{"Cardiology", "Neurology", "Pediatrics", "Orthopedics", "Dermatology", "Oncology", "Psychiatry"};
         Random rand = new Random();
         return specialties[rand.nextInt(specialties.length)];
     }
@@ -24,13 +24,19 @@ public class DoctorInfo {
     }
 
     public void displayInfo() {
-        System.out.println("Doctor ID: " + doctorID);
-        System.out.println("Name: " + doctorName);
-        System.out.println("Specialization: " + specialization);
+        System.out.println("Doctor ID: " + this.doctorID);
+        System.out.println("Name: " + this.doctorName);
+        System.out.println("Specialization: " + this.specialization);
         System.out.print("Availability: ");
-        for (String time : availability) {
+        String[] var1 = this.availability;
+        int var2 = var1.length;
+
+        for(int var3 = 0; var3 < var2; ++var3) {
+            String time = var1[var3];
             System.out.print(time + " ");
         }
+
         System.out.println();
     }
 }
+
