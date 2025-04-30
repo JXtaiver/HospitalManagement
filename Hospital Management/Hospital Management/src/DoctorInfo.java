545b4dcf5e712a1bec5b65f5c64293491854fd3c
ultradan-beep <55604224+ultradan-beep@users.noreply.github.com>
2025-04-29 22:45:23 -0400

    Update DoctorInfo.java

--- Hospital Management/Hospital Management/src/DoctorInfo.java
+++ Hospital Management/Hospital Management/src/DoctorInfo.java
@@ -1,22 +1,22 @@
-import java.util.Random;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class DoctorInfo {
     private int doctorID;
     private String doctorName;
     private String[] availability;
     private String specialization;
+    static Scanner scan = new Scanner(System.in);
 
-    public DoctorInfo(int doctorID, String doctorName, String[] availability) {
+    public DoctorInfo(int doctorID, String doctorName, String[] availability, String specialization) {
         this.doctorID = doctorID;
         this.doctorName = doctorName;
         this.availability = availability;
-        this.specialization = this.generateRandomSpecialization();
+        this.specialization = specialization;
     }
 
-    private String generateRandomSpecialization() {
-        String[] specialties = new String[]{"Cardiology", "Neurology", "Pediatrics", "Orthopedics", "Dermatology", "Oncology", "Psychiatry"};
-        Random rand = new Random();
-        return specialties[rand.nextInt(specialties.length)];
+    public String getSpecialization() {
+        return specialization;
     }
 
     public void updateAvailability(String[] newAvailability) {
@@ -24,19 +24,68 @@ public class DoctorInfo {
     }
 
     public void displayInfo() {
-        System.out.println("Doctor ID: " + this.doctorID);
-        System.out.println("Name: " + this.doctorName);
-        System.out.println("Specialization: " + this.specialization);
+        System.out.println("Doctor ID: " + doctorID);
+        System.out.println("Name: " + doctorName);
+        System.out.println("Specialization: " + specialization);
         System.out.print("Availability: ");
-        String[] var1 = this.availability;
-        int var2 = var1.length;
-
-        for(int var3 = 0; var3 < var2; ++var3) {
-            String time = var1[var3];
+        for (String time : availability) {
             System.out.print(time + " ");
         }
-
         System.out.println();
     }
+
+    public void Doctor_Menu(HashMap<Integer, DoctorInfo> doctors) {
+        while (true) {
+            System.out.println("\n Select an Option: ");
+            System.out.println(" 1: Display Doctor List");
+            System.out.println(" 2: Display Doctor Info");
+            System.out.println(" 3: Update Availability");
+            System.out.println(" 4: Exit to Main Menu");
+            int type = scan.nextInt();
+
+            switch (type) {
+                case 1:
+                    for (Integer id : doctors.keySet()) {
+                        System.out.println("Doctor ID: " + id + ", Name: " + doctors.get(id).doctorName);
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("Enter Doctor ID: ");
+                    int docId = scan.nextInt();
+                    if (doctors.containsKey(docId)) {
+                        doctors.get(docId).displayInfo();
+                    } else {
+                        System.out.println("Doctor not found.");
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Enter Doctor ID: ");
+                    int updateId = scan.nextInt();
+                    scan.nextLine();
+                    if (doctors.containsKey(updateId)) {
+                        System.out.println("Enter new availability (comma-separated): ");
+                        String[] newTimes = scan.nextLine().split(",");
+                        for (int i = 0; i < newTimes.length; i++) {
+                            newTimes[i] = newTimes[i].trim();
+                        }
+                        doctors.get(updateId).updateAvailability(newTimes);
+                        System.out.println("Availability updated.");
+                    } else {
+                        System.out.println("Doctor not found.");
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Returning to Main Menu...");
+                    return;
+
+                default:
+                    System.out.println("Invalid Option, Try again.");
+            }
+        }
+    }
 }
+
 
